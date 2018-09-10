17d7d95748a1fdb772e7ede77fa2d4b9705b0977
Torin Huff <torinhuff@yahoo.com>
2018-09-09 18:36:26 -0700

    first jquery functions. This will alert user when clicking on any elements.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+jQuery("h1").click(function() {
+  alert("This is a header.");
+  alert("I TOLD YOU THIS IS A HEADER.");
+});
+
+jQuery("p").click(function() {
+  alert("This is a paragraph.");
+});
+
+jQuery("img").click(function() {
+  alert("This is an image.");
+});
